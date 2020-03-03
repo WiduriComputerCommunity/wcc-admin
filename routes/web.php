2f41a43768cafff44adb9e6d92af782f80202f9d
firasmpratama <firasmpratama17@gmail.com>
2020-03-03 16:57:38 +0700

    update routes user managements

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Auth::routes();
-
 Route::get('/', 'AuthController@index');
 Route::get('register', 'AuthController@register');
 
+//Auth
+Route::get('login', 'AuthController@login');
+Route::get('login', 'AuthController@attempt');
+Route::get('logout', 'AuthController@logout');
+
+//User
+Route::group(['prefix' => 'api/v1/users', 'middleware' => 'auth.basic'], function () {
+    Route::get('/', 'Api\UserController@index');
+    Route::get('/{id}', 'Api\UserController@detail');
+    Route::put('/edit/{id}', 'Api\UserController@edit');
+    Route::post('/register', 'Api\UserController@register');
+    Route::delete('/{id}', 'Api\UserController@destroy');
+});
 
 
 
