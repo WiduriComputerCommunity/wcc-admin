a425360a52dde7541c3ae8e26311281f2c7055d7
firasmpratama <firasmpratama17@gmail.com>
2020-03-11 19:49:33 +0700

    change language to Indonesia

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -424,12 +424,12 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Yakin ingin keluar?</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-body">Pilih "Logout" apabila anda ingin mengakhiri.</div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
           <a class="btn btn-primary" href="{{url ('logout')}}">Logout</a>
