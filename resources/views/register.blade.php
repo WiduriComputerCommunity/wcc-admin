fb03bbc1321c90177c0fa967f486750a47c26b7f
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-12 19:12:42 +0700

    tambah md

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -58,7 +58,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-6 offset-3">
+            <div class="col-md-6 offset-md-3">
 
               <div class="card" style="width: 18rem;">
                 <img class="card-img-top" src="images/WCC-LOGO.png" alt="Background">
