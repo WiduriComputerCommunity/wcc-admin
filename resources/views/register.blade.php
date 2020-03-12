e3d3f075b0a890ccc761a896a2b76c0849bd120e
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-12 17:53:43 +0700

    Warna text

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -147,7 +147,7 @@
                                 <input id="notelp" type="tel" name="notelp" class="form-control">
 
                                   <?php if ($errors->has('notelp')): ?>
-                                    <span class="help-block">
+                                    <span class="help-block" style="color:#FF0000;">
                                       <strong>
                                         <?php
                                           if ($errors->first('notelp') == "The notelp format is invalid.")
