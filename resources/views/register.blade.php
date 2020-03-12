ed12380ca7a13517642f0ca5f28d1d424190d542
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-12 18:34:31 +0700

    Bootstrap 4 Navbar

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -37,52 +37,24 @@
     </style>
 </head>
 <body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top" style="background-color: #337ab7">
-        <div class="container">
-            <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Widuri Computer Community</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" style="color: white;" href="{{ url('/') }}">
-                    Widuri Computer Community
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <!-- <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/') }}">Home</a></li>
-                </ul> -->
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <!-- <li><a href="{{ url('/login') }}">Login</a></li> -->
-                        <li><a style="color: white;" href="{{ url('/') }}">Login</a></li>
-                        <li><a style="color: white;" href="{{ url('register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/') }}">Login</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="{{ url('register') }}">Register</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
     <div class="container">
         <div class="row">
