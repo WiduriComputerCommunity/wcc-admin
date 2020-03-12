bac10bc413b88e9d8c698966153aa79f3e96e212
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-12 20:12:56 +0700

    change radius

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -60,7 +60,7 @@
         <div class="row">
             <div class="col-md-6 offset-md-3">
 
-              <div class="card my-5 shadow" style="border-radius:.40rem;">
+              <div class="card my-5 shadow" style="border-radius:10px !important;">
 
                 <div class="card-header" style="background:url('images/wccSlider.jpg');">
                   <h5 class="card-title text-white">Register to start your session</h5>
