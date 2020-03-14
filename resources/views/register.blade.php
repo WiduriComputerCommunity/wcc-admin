4cacbd192ca269be7c3c55b8e7c7ea1062ccc1f7
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-14 13:34:24 +0700

    Fix cropped header

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -53,7 +53,7 @@
 
               <div class="card my-5 shadow" style="border-radius: 30px !important;">
 
-                <div class="card-header" style="border-radius: 30px 30px 0 0 !important; background:url('images/wccSlider.jpg'); height:80px;">
+                <div class="card-header" style="border-radius: 30px 30px 0 0 !important; background:url('images/wccSlider.jpg');">
                   <h2 class="card-title text-white text-center display-2">Register</h2>
                 </div>
 
